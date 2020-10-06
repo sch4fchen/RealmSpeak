8eec5723bbbdbbcc4a0d41fbc4f7d671e8194769
sch4fchen <sch4fchen>
2020-10-06 22:03:07 +0200

    Fix QuestRequirementOpen when opening a chest with activateItem

--- magic_realm/utility/components/source/com/robin/magic_realm/components/quest/requirement/QuestRequirementOpen.java
+++ magic_realm/utility/components/source/com/robin/magic_realm/components/quest/requirement/QuestRequirementOpen.java
@@ -27,6 +27,7 @@ import com.robin.game.objects.GameObject;
 import com.robin.magic_realm.components.quest.CharacterActionType;
 import com.robin.magic_realm.components.utility.Constants;
 import com.robin.magic_realm.components.wrapper.CharacterWrapper;
+import com.sun.org.apache.xerces.internal.xs.datatypes.ObjectList;
 
 public class QuestRequirementOpen extends QuestRequirement {
 	private static Logger logger = Logger.getLogger(QuestRequirementOpen.class.getName());
@@ -40,21 +41,30 @@ public class QuestRequirementOpen extends QuestRequirement {
 
 	@Override
 	protected boolean testFulfillsRequirement(JFrame frame, CharacterWrapper character, QuestRequirementParams reqParams) {
-		if (characterHasToOpenIt() && reqParams.actionType != CharacterActionType.Open && reqParams.actionType != CharacterActionType.SearchTable) {
+		if (characterHasToOpenIt() && reqParams.actionType != CharacterActionType.Open && reqParams.actionType != CharacterActionType.ActivatingItem && reqParams.actionType != CharacterActionType.SearchTable) {
 			return false;
 		}
 
 		String regex = getRegExFilter();
 				
-			if (characterHasToOpenIt() && (reqParams.actionType == CharacterActionType.Open || reqParams.actionType == CharacterActionType.SearchTable)) {
+			if (characterHasToOpenIt() && (reqParams.actionType == CharacterActionType.Open || reqParams.actionType == CharacterActionType.ActivatingItem || reqParams.actionType == CharacterActionType.SearchTable)) {
 				if (regex != null && regex.trim().length() > 0) {
 					Pattern pattern = Pattern.compile(regex);
-					if (!pattern.matcher(reqParams.targetOfSearch.getName()).find()) {
-						logger.fine(reqParams.targetOfSearch.getName()+" does not match regex /"+regex+"/");
+					if (reqParams.actionType == CharacterActionType.Open || reqParams.actionType == CharacterActionType.SearchTable) {
+						if (!pattern.matcher(reqParams.targetOfSearch.getName()).find()) {
+							logger.fine(reqParams.targetOfSearch.getName()+" does not match regex /"+regex+"/");
+						}
 					}
-					else {
-						return true;
+					else if (reqParams.actionType == CharacterActionType.ActivatingItem) {
+						GameObject item = reqParams.objectList.get(0);
+						String itemName = item != null ? item.getName() : "Item";
+							if (item == null || !pattern.matcher(itemName).find()) {
+								logger.fine(itemName+" does not match regex /"+regex+"/");
+							}
+						}
 					}
+				else {
+					return true;
 				}
 			}
 			else {
