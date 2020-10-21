7a17b1ce99427e038ffb4ad84b520434ad020385
sch4fchen <sch4fchen>
2020-10-21 21:25:40 +0200

    QuestRewardSummonMonster: Option to summon monster to a location

--- magic_realm/applications/RealmQuestBuilder/source/com/robin/magic_realm/RealmQuestBuilder/QuestRewardEditor.java
+++ magic_realm/applications/RealmQuestBuilder/source/com/robin/magic_realm/RealmQuestBuilder/QuestRewardEditor.java
@@ -186,10 +186,13 @@ public class QuestRewardEditor extends QuestBlockEditor {
 				list.add(new QuestPropertyBlock(QuestRewardStripInventory.STRIP_GOLD, "Strip Gold", FieldType.Boolean));
 				break;
 			case SummonGuardian:
-				list.add(new QuestPropertyBlock(QuestRequirementLocation.LOCATION, "Summon Guardian for ", FieldType.GameObjectWrapperSelector, quest.getLocations().toArray()));
+				list.add(new QuestPropertyBlock(QuestRewardSummonGuardian.LOCATION, "Summon Guardian for ", FieldType.GameObjectWrapperSelector, quest.getLocations().toArray()));
 				break;
 			case SummonMonster:
 				list.add(new QuestPropertyBlock(QuestRewardSummonMonster.MONSTER_NAME, "Monster", FieldType.CompanionSelector, getAllCompanionKeyValues()));
+				list.add(new QuestPropertyBlock(QuestRewardSummonMonster.SUMMON_TO_LOCATION, "Summon to location", FieldType.Boolean));
+				list.add(new QuestPropertyBlock(QuestRewardSummonMonster.RANDOM_LOCATION, "Random clearing of location", FieldType.Boolean));
+				list.add(new QuestPropertyBlock(QuestRewardSummonMonster.LOCATION, "Location", FieldType.GameObjectWrapperSelector, quest.getLocations().toArray()));
 				break;
 			case SummonRoll:
 				list.add(new QuestPropertyBlock(QuestRewardSummonRoll.DIE_ROLL, "Die roll", FieldType.StringSelector, DieRollType.values()));
--- magic_realm/utility/components/source/com/robin/magic_realm/components/quest/reward/QuestRewardSummonMonster.java
+++ magic_realm/utility/components/source/com/robin/magic_realm/components/quest/reward/QuestRewardSummonMonster.java
@@ -17,19 +17,30 @@
  */
 package com.robin.magic_realm.components.quest.reward;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.logging.Logger;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 import com.robin.game.objects.GameObject;
+import com.robin.general.util.RandomNumber;
 import com.robin.magic_realm.components.RealmComponent;
+import com.robin.magic_realm.components.attribute.TileLocation;
 import com.robin.magic_realm.components.quest.QuestConstants;
+import com.robin.magic_realm.components.quest.QuestLocation;
+import com.robin.magic_realm.components.quest.QuestStep;
 import com.robin.magic_realm.components.utility.Constants;
 import com.robin.magic_realm.components.utility.TemplateLibrary;
 import com.robin.magic_realm.components.wrapper.CharacterWrapper;
 
 public class QuestRewardSummonMonster extends QuestReward {
-	
+	private static Logger logger = Logger.getLogger(QuestStep.class.getName());
 	public static final String MONSTER_NAME = "_mn";
+	public static final String SUMMON_TO_LOCATION = "_summon_loc";
+	public static final String RANDOM_LOCATION = "_rnd_loc";
+	public static final String LOCATION = "_loc";
 	
 	public QuestRewardSummonMonster(GameObject go) {
 		super(go);
@@ -40,7 +51,29 @@ public class QuestRewardSummonMonster extends QuestReward {
 		GameObject monster = TemplateLibrary.getSingleton().createCompanionFromTemplate(getGameData(),template);
 		monster.removeThisAttribute(Constants.COMPANION);
 		monster.setThisAttribute(Constants.SUMMONED);
-		character.getCurrentLocation().clearing.add(monster,null);	
+		if (locationOnly()) {
+			QuestLocation loc = getQuestLocation();
+			ArrayList<TileLocation> validLocations = new ArrayList<TileLocation>();
+			validLocations = loc.fetchAllLocations(frame, character, getGameData());
+			if(validLocations.isEmpty()) {
+				logger.fine("QuestLocation "+loc.getName()+" doesn't have any valid locations!");
+				return;
+			}
+			if (randomLocation()) {
+				int random = RandomNumber.getRandom(validLocations.size());
+				TileLocation tileLocation = validLocations.get(random);
+				tileLocation.clearing.add(monster,null);
+			}
+			else {
+				for (TileLocation location : validLocations) {
+					GameObject summonMonster = monster.copy();
+					location.clearing.add(summonMonster, null);
+				}
+			}
+			return;
+		}
+		
+		character.getCurrentLocation().clearing.add(monster,null);
 	}
 	
 	public ImageIcon getIcon() {
@@ -50,18 +83,59 @@ public class QuestRewardSummonMonster extends QuestReward {
 	}
 	
 	public String getDescription() {
-		return getMonsterKeyName()+" is placed in the characters clearing.";
+		if (locationOnly()) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(getMonsterKeyName()+" is summoned in ");
+			if (randomLocation()) {
+				sb.append("a random clearing of ");
+			}
+			sb.append(getQuestLocation().getName());
+			return sb.toString();
+		}
+		return getMonsterKeyName()+" is summoned in the characters clearing.";
 	}
 
 	public RewardType getRewardType() {
 		return RewardType.SummonMonster;
 	}
 	
-	public String getMonsterKeyName() {
+	private String getMonsterKeyName() {
 		return getString(QuestConstants.KEY_PREFIX+MONSTER_NAME);
 	}
 	
-	public String getMonsterQuery() {
+	private String getMonsterQuery() {
 		return getString(QuestConstants.VALUE_PREFIX+MONSTER_NAME);
+	}
+	
+	private boolean locationOnly() {
+		return getBoolean(SUMMON_TO_LOCATION);
+	}
+	
+	private boolean randomLocation() {
+		return getBoolean(RANDOM_LOCATION);
+	}
+	
+	public boolean usesLocationTag(String tag) {
+		QuestLocation loc = getQuestLocation();
+		return loc!=null && tag.equals(loc.getName());
+	}
+	
+	public QuestLocation getQuestLocation() {
+		String id = getString(LOCATION);
+		if (id!=null) {
+			GameObject go = getGameData().getGameObject(Long.valueOf(id));
+			if (go!=null) {
+				return new QuestLocation(go);
+			}
+		}
+		return null;
+	}
+
+	public void setQuestLocation(QuestLocation location) {
+		setString(LOCATION,location.getGameObject().getStringId());
+	}
+	
+	public void updateIds(Hashtable<Long, GameObject> lookup) {
+		updateIdsForKey(lookup,LOCATION);
 	}
 }
