09e3077bbf89a8c64a73dc448c707fca10af055c
sch4fchen <inferno-dragon@web.de>
2021-04-17 22:15:13 +0200

    Followers stop following at end a characters turn (and summon monsters)

--- magic_realm/utility/components/source/com/robin/magic_realm/components/wrapper/CharacterWrapper.java
+++ magic_realm/utility/components/source/com/robin/magic_realm/components/wrapper/CharacterWrapper.java
@@ -3090,6 +3090,7 @@ public class CharacterWrapper extends GameObjectWrapper {
 						GameObject go = getGameObject().getGameData().getGameObject(Long.valueOf(id));
 						CharacterWrapper nextFollower = new CharacterWrapper(go);
 						nextFollower.setNoSummon(true);
+						
 					}
 					
 					// Summon monsters now.
