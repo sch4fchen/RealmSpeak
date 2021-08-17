d9a1364f453c55261546a841df5beeb1ef34831a
sch4fchen <inferno-dragon@web.de>
2021-08-17 17:31:55 +0200

    Fix NullPointExcpetion for SpellCreator if spell is not found

--- magic_realm/utility/components/source/com/robin/magic_realm/components/quest/SpellCreator.java
+++ magic_realm/utility/components/source/com/robin/magic_realm/components/quest/SpellCreator.java
@@ -17,12 +17,15 @@
  */
 package com.robin.magic_realm.components.quest;
 
+import com.robin.game.objects.GameObject;
 import com.robin.magic_realm.components.wrapper.CharacterWrapper;
 import com.robin.magic_realm.components.wrapper.SpellWrapper;
 
 public class SpellCreator {
 	public static SpellWrapper CreateSpellWrapper(String spell, CharacterWrapper character) {
-		SpellWrapper spellWrapper = new SpellWrapper(character.getGameData().getGameObjectByNameIgnoreCase(spell));
+		GameObject spellGo = character.getGameData().getGameObjectByNameIgnoreCase(spell);
+		if (spellGo == null) return null;
+		SpellWrapper spellWrapper = new SpellWrapper(spellGo);
 		spellWrapper.setString(SpellWrapper.CASTER_ID, String.valueOf(character.getGameObject().getId()));
 		spellWrapper.setString(SpellWrapper.SPELL_ALIVE,"");
 		return spellWrapper;
