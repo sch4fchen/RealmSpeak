7a11a39323a3a97a486aeaddc30f6545f21661ad
sch4fchen <inferno-dragon@web.de>
2022-07-31 19:29:07 +0200

    Increase version to 1.1.6.4

--- magic_realm/utility/components/source/com/robin/magic_realm/components/utility/Constants.java
+++ magic_realm/utility/components/source/com/robin/magic_realm/components/utility/Constants.java
@@ -43,7 +43,7 @@ public class Constants {
 	public static final String DIE_ROLL_LOG = "_die_roll_log_";
 	
 	public static final String APPLICATION_NAME = "Realm Speak";
-	public static final String REALM_SPEAK_VERSION = "1.1.6.3";
+	public static final String REALM_SPEAK_VERSION = "1.1.6.4";
 	public static final String REALM_SPEAK_IMAGES_VERSION = "1.1.5.7";
 	
 	public static final String PLAYER_TO_PLACE = "p_2_p";
