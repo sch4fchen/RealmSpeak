205468b063166e7d0468c10b62fdd0241dd4802c
sch4fchen <inferno-dragon@web.de>
2022-11-04 12:50:04 +0100

    Adapt reward editor for quest reward item random choice

--- magic_realm/applications/RealmQuestBuilder/source/com/robin/magic_realm/RealmQuestBuilder/QuestRewardEditor.java
+++ magic_realm/applications/RealmQuestBuilder/source/com/robin/magic_realm/RealmQuestBuilder/QuestRewardEditor.java
@@ -178,6 +178,7 @@ public class QuestRewardEditor extends QuestBlockEditor {
 				list.add(new QuestPropertyBlock(QuestRewardItem.ITEM_DESC, "Description", FieldType.TextLine));
 				list.add(new QuestPropertyBlock(QuestRewardItem.ITEM_CHITTYPES, "Item Type Restriction", FieldType.ChitType));
 				list.add(new QuestPropertyBlock(QuestRewardItem.ITEM_REGEX, "Item RegEx", FieldType.Regex));
+				list.add(new QuestPropertyBlock(QuestRewardItem.RANDOM, "Select random item (no player choosing)", FieldType.Boolean));
 				list.add(new QuestPropertyBlock(QuestRewardItem.FORCE_DEACTIVATION, "Force losing (e.g. cursed)", FieldType.Boolean));
 				list.add(new QuestPropertyBlock(QuestRewardItem.MARK_ITEM, "Mark item", FieldType.Boolean));
 				list.add(new QuestPropertyBlock(QuestRewardItem.REQ_MARK, "Requires mark", FieldType.Boolean));
