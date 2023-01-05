cc15f39485f83172f537524b2e790638ed57d89d
sch4fchen <inferno-dragon@web.de>
2023-01-05 11:59:20 +0100

    Add credits for super realm graphics

--- magic_realm/applications/RealmSpeak/source/com/robin/magic_realm/RealmSpeak/RealmSpeakFrame.java
+++ magic_realm/applications/RealmSpeak/source/com/robin/magic_realm/RealmSpeak/RealmSpeakFrame.java
@@ -497,6 +497,9 @@ public class RealmSpeakFrame extends JFrameWithStatus {
 		scroller.addLine(new ScrollLine());
 		scroller.addLine(new ScrollLine("Super Realm",subheader,subheaderColor));
 		scroller.addLine(new ScrollLine("Anomalous Host",listing,listingColor));
+		scroller.addLine(new ScrollLine("Super Realm Graphics",subheader,subheaderColor));
+		scroller.addLine(new ScrollLine("Casey Benn",listing,listingColor));
+		scroller.addLine(new ScrollLine("Psyrek",listing,listingColor));
 		scroller.addLine(new ScrollLine());
 		scroller.addLine(new ScrollLine("Expansion Monster Graphics",subheader,subheaderColor));
 		scroller.addLine(new ScrollLine("Vrin Thomas",listing,listingColor));
