2f01b18c3853df35e9398d4d30d24f1adc924848
sch4fchen <inferno-dragon@web.de>
2023-07-11 14:36:04 +0200

    Exorcise spell should kill the Imp

--- magic_realm/utility/components/source/com/robin/magic_realm/components/effect/ExorciseEffect.java
+++ magic_realm/utility/components/source/com/robin/magic_realm/components/effect/ExorciseEffect.java
@@ -12,7 +12,7 @@ public class ExorciseEffect implements ISpellEffect {
 	public void apply(SpellEffectContext context) {
 		CombatWrapper combat = context.getCombatTarget();
 		
-		if (context.Target.getGameObject().hasThisAttribute(Constants.DEMON)||context.Target.getGameObject().hasThisAttribute(Constants.DEVIL)||context.Target.getGameObject().hasThisAttribute(Constants.VAMPIRE)||context.Target.getGameObject().hasThisAttribute(Constants.SUCCUBUS)) {
+		if (context.Target.getGameObject().hasThisAttribute(Constants.DEMON)||context.Target.getGameObject().hasThisAttribute(Constants.IMP)||context.Target.getGameObject().hasThisAttribute(Constants.DEVIL)||context.Target.getGameObject().hasThisAttribute(Constants.VAMPIRE)||context.Target.getGameObject().hasThisAttribute(Constants.SUCCUBUS)) {
 			combat.setKilledBy(context.Caster);
 			combat.setKilledLength(18);
 			combat.setKilledSpeed(context.Spell.getAttackSpeed());
